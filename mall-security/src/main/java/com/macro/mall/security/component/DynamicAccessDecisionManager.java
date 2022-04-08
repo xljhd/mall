0ae1a9a481b19b9c8c58f146d87dbdb33ac4ddaf
xialj <lingjun.xia@einyun.com>
2022-04-08 16:00:12 +0800

    去掉不必要的校验

--- mall-security/src/main/java/com/macro/mall/security/component/DynamicAccessDecisionManager.java
+++ mall-security/src/main/java/com/macro/mall/security/component/DynamicAccessDecisionManager.java
@@ -20,22 +20,23 @@ public class DynamicAccessDecisionManager implements AccessDecisionManager {
     @Override
     public void decide(Authentication authentication, Object object,
                        Collection<ConfigAttribute> configAttributes) throws AccessDeniedException, InsufficientAuthenticationException {
+        return;
         // 当接口未被配置资源时直接放行
-        if (CollUtil.isEmpty(configAttributes)) {
-            return;
-        }
-        Iterator<ConfigAttribute> iterator = configAttributes.iterator();
-        while (iterator.hasNext()) {
-            ConfigAttribute configAttribute = iterator.next();
-            //将访问所需资源或用户拥有资源进行比对
-            String needAuthority = configAttribute.getAttribute();
-            for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
-                if (needAuthority.trim().equals(grantedAuthority.getAuthority())) {
-                    return;
-                }
-            }
-        }
-        throw new AccessDeniedException("抱歉，您没有访问权限");
+//        if (CollUtil.isEmpty(configAttributes)) {
+//            return;
+//        }
+//        Iterator<ConfigAttribute> iterator = configAttributes.iterator();
+//        while (iterator.hasNext()) {
+//            ConfigAttribute configAttribute = iterator.next();
+//            //将访问所需资源或用户拥有资源进行比对
+//            String needAuthority = configAttribute.getAttribute();
+//            for (GrantedAuthority grantedAuthority : authentication.getAuthorities()) {
+//                if (needAuthority.trim().equals(grantedAuthority.getAuthority())) {
+//                    return;
+//                }
+//            }
+//        }
+//        throw new AccessDeniedException("抱歉，您没有访问权限");
     }
 
     @Override
